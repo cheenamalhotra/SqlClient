2738fdd9a6a9c6f20e308da95faf01d03f97c1a9
Cheena Malhotra <v-chmalh@microsoft.com>
2020-01-17 16:21:47 -0800

    Testing Transitive Reference

---
+++ src/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using(SqlConnection con = new SqlConnection("Server=localhost;Integrated Security = true;"))
+            {
+                con.Open();
+                Console.WriteLine("Connected!");
+            }
+        }
+    }
+}
